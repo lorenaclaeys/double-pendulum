7ac1b4ac58138d04a43f98a7453852311f8e2220
lorenaclaeys <80050872+lorenaclaeys@users.noreply.github.com>
2021-04-29 21:10:44 +0200

    Update lyapunov.py
    final

--- lyapunov.py
+++ lyapunov.py
@@ -5,48 +5,19 @@ import matplotlib.pyplot as plt
 from math import log
 import numpy as np
 
-######## parameters
-#gravity(m/s²)
-g = 9.81
-#constants for the solver
-h = 0.01 #time step
-t_max = 20 #(s)
-#masses (kg)
-m1 = 1.
-m2 = 1.
-# lenght (m)
-l=1.
-###1st pendulum
-#angle & derivatives (rad)
-th1 = (math.pi)
-th2 = .4*(math.pi)
-dth1 = 0
-dth2 = 0
-#perturbation (rad)
-delta1 = 0.001
-delta2 = 0.001
-###2nd pendulum
-#angle (rad) & derivatives
-th1A = th1 + delta1
-th2A = th2 + delta2
-dth1A = 0
-dth2A = 0
 
-#data from solver
-THETA, T = solver.pendulum(solver.p_derivatives, th1, th2, 0, 0, t_max, h, m1, m2, g, l)
-THETAA, T = solver.pendulum(solver.p_derivatives, th1 + delta1, th2 + delta2, 0, 0, t_max, h, m1, m2, g, l)
-
-
-# plt.plot(n,THETA[:,0]%2*math.pi)
+#trajectories
+#plt.plot(n,THETA[:,0]%2*math.pi)
 #plt.plot(T,THETAA[:,0]%2*math.pi)
 #plt.plot(T,THETA[:,1]%2*math.pi)
 #plt.plot(T,THETAA[:,1]%2*math.pi)
-# plt.show()
+#plt.show()
 
 
 
-########energy
+#energy
 def energy(theta, m1, m2, g, l):
+    """function that returns the energy of the 2 double pendulums"""
     th1 = theta[0]
     th2 = theta[1]
     dth1 = theta[2]
@@ -55,107 +26,12 @@ def energy(theta, m1, m2, g, l):
     K = .5*m1*(l*dth1)**2 + .5*m2*((l*dth1)**2 + (l*dth2)**2 + 2*l*l*dth1*dth2*math.cos(th1-th2))
     return K + V
 
-#graph for energy
-H = np.zeros(len(T))
-for (i,t) in enumerate(T):
-    H[i] = energy(THETA[i,:], m1, m2, g, l)
-print(H[-1])
-plt.plot(T,H)
-plt.title("energy")
-plt.xlabel("t")
-plt.ylabel("H")
-plt.show()
-
-#graph two double pendulums
-ani = graph.grapher_two(l,THETA, THETAA, h)
-plt.show()
 
 
 
-########lyapunov exponent
-#initial condition
-delta = THETAA[-1,:] - THETA[-1,:]
-#normalised initial condition
-delta0 = delta/np.linalg.norm(delta)
 #lyapunov exponent
-L=0
-n = 2001
-lyap = np.zeros(n)
-i=0
-while i < n:
-    THETAA, _ = solver.pendulum(solver.p_derivatives, THETA[-1, 0]+delta0[0], THETA[-1, 1]+delta0[1], THETA[-1, 2]+delta0[2], THETA[-1, 3]+delta0[3], h, h, m1, m2, g, l)
-    THETA, _ = solver.pendulum(solver.p_derivatives, THETA[-1, 0], THETA[-1, 1], THETA[-1, 2], THETA[-1, 3], h, h, m1, m2, g, l)
-    #renormalisation
-    delta = THETAA[-1,:] - THETA[-1,:]
-    norm = np.linalg.norm(delta)
-    L += log(norm)
-    delta0 = delta/norm
-    i += 1
-    lyap[i-1] = L/i/h
-
-
-#maximal lyapunov exponent (log)
-lambda_max = L/n/h #
-print("the maximal lyapunov exponent is",lambda_max)
-#graph
-lyap2 = lyap
-plt.title("lyapunov exponent")
-plt.xlabel("t")
-plt.ylabel("lyapunov exponent")
-plt.plot(range(n),lyap2)
-plt.show()
-
-
-
-
-
-
-#question 6
-N = 100
-Lyapunov = []
-Energie = []
-for k in range(N):
-    th1 = (1*math.pi)/ (N-k)
-    th2 = 0
-    THETA, T = Solver.pendulum(Solver.p_derivatives, th1, th2, 0, 0, t_max, h, m1, m2, g, l)
-    THETAA, T = Solver.pendulum(Solver.p_derivatives, th1 + delta1, th2 + delta2, 0, 0, t_max, h, m1, m2, g, l)
-    H = np.zeros(len(T))
-    for (i,t) in enumerate(T):
-        H[i] = energy(THETA[i,:], m1, m2, g, l)
-    H_average = np.amin(H)
-    Energie.append(H_average)
-    Lyapunov.append(lyap(THETAA, THETA, delta))
-
-plt.xlabel("Energy")
-plt.ylabel("Lyapunov exponent")
-plt.plot(Energie, Lyapunov)
-plt.show()
-
-#Ancienne question 6 que tu peux supprimer
-
-th1= np.linspace(0,math.pi*0.5,90)
-H_tot = np.zeros(int(90/1))
-H = np.zeros(len(T))
-for k in range(0,90,1): #1 to 10
-    #data from solver
-    THETA, T = solver.pendulum(solver.p_derivatives, th1[k], th2, 0, 0, t_max, h, m1, m2, g, l)
-    THETAA, T = solver.pendulum(solver.p_derivatives, th1[k] + delta1, th2 + delta2, 0, 0, t_max, h, m1, m2, g, l)
-    for (i,t) in enumerate(T):
-        H[i] = energy(THETA[i,:], m1, m2, g, l)
-    H_tot[k] = H[0]
-
-lambda_max_tot= np.zeros(90)
-for k in range(0,90,1): #1 to 10
-    THETA, T = solver.pendulum(solver.p_derivatives, th1[k], th2, 0, 0, t_max, h, m1, m2, g, l)
-    THETAA, T = solver.pendulum(solver.p_derivatives, th1[k] + delta1, th2 + delta2, 0, 0, t_max, h, m1, m2, g, l)
-    ########lyapunov exponent
-    #initial condition
-    delta = THETAA[-1,:] - THETA[-1,:]
-    #normalised initial condition
-    delta0 = delta/np.linalg.norm(delta)
-    #lyapunov exponent
-    L=0
-    n = 2001
+def lyap(L, n ,delta0 ,delta,THETA, THETAA, h, m1, m2, g, l):
+    """fuction that returns the maximal lyapunov exponent and a vector containing the evolution of the lyapunov coefficient""" 
     lyap = np.zeros(n)
     i=0
     while i < n:
@@ -170,8 +46,26 @@ for k in range(0,90,1): #1 to 10
         lyap[i-1] = L/i/h
     #maximal lyapunov exponent (log)
     lambda_max = L/n/h
-    lambda_max_tot[k]= lambda_max
-print("h",H_tot,"l",lambda_max_tot)
+    return lambda_max, lyap
 
-plt.plot(th1,lambda_max_tot)
-plt.show()
+
+#lyapunov evolution
+Lyapunov = []
+Energy = []
+def lyap_evolution(delta1, delta2, delta0, delta, L, n, N, t_max, h, m1, m2, g, l):
+    """function that gives the evolution of the maximal lyapunov coefficient in function of the energy"""
+    for k in range(N):
+        #we make our energy vary in function of theta1
+        th1 = (1*math.pi)/ (N-k)
+        th2 = 0
+        THETA, T = solver.pendulum(solver.p_derivatives, th1, th2, 0, 0, t_max, h, m1, m2, g, l)
+        THETAA, T = solver.pendulum(solver.p_derivatives, th1 + delta1, th2 + delta2, 0, 0, t_max, h, m1, m2, g, l)
+        H = np.zeros(len(T))
+        for (i,t) in enumerate(T):
+            H[i] = energy(THETA[i,:], m1, m2, g, l)
+        H_average = np.amin(H)
+        Energy.append(H_average)
+        lambda_max,_ = lyap(L, n ,delta0 ,delta,THETA, THETAA, h, m1, m2, g, l)
+        Lyapunov.append(lambda_max)
+    return Energy, Lyapunov
+
